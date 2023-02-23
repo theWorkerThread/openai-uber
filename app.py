f90221af21a3501e89d2de4f999b9919fb3509ea
Sagar Tyagi <sagar.tyagi@uber.com>
2023-02-23 17:14:08 +0530

    Updating UI

--- app.py
+++ app.py
@@ -5,25 +5,23 @@ from flask import Flask, redirect, render_template, request, url_for
 
 app = Flask(__name__)
 openai.api_key = os.getenv("OPENAI_API_KEY")
-print("KEY:",openai.api_key)
 
 @app.route("/", methods=("GET", "POST"))
 def index():
     if request.method == "POST":
-        animal = request.form["animal"]
+        text = request.form["text"]
         response = openai.Completion.create(
             model="text-davinci-003",
-            prompt=generate_prompt(animal),
+            prompt=generate_prompt(text),
             temperature=0,
         )
-        print("RESPONSE:" , response)
         return redirect(url_for("index", result=response.choices[0].text))
 
     result = request.args.get("result")
     return render_template("index.html", result=result)
 
 
-def generate_prompt(animal):
+def generate_prompt(text):
     return """Extract address from the Text
 
 Text: in your headed to was the the Fort Lauderdale Hollywood Airport correct
@@ -34,5 +32,5 @@ Text: That would be 411, 98th street, No, I think it's 5th street
 Address: 411,5th street
 Text: {}
 Address:""".format(
-        animal.capitalize()
+        text.capitalize()
     )
